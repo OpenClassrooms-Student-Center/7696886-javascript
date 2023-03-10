9916db2280424d807407bd9b4390c32ea86a4cfa
dpierru <david.pierru@oneira.net>
2023-03-10 18:18:02 +0100

    Création du bouton "partager" et gestion de la popup de partage.

---
+++ scripts/popup.js
@@ -0,0 +1,47 @@
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les fonctions nécessaires à l'affichage et à la 
+ * fermeture de la popup de partage. 
+ * 
+ *********************************************************************************/
+
+
+/**
+ * Cette fonction affiche la popup pour partager son score. 
+ */
+function afficherPopup() {
+    let popupBackground = document.querySelector(".popupBackground")
+    popupBackground.classList.add("active")
+}
+
+/**
+ * Cette fonction cache la popup pour partager son score. 
+ */
+function cacherPopup() {
+    let popupBackground = document.querySelector(".popupBackground")
+    popupBackground.classList.remove("active")
+}
+
+/**
+ * Cette fonction initialise les écouteurs d'événements qui concernent 
+ * l'affichage de la popup. 
+ */
+function initAddEventListenerPopup() {
+    // On écoute le click sur le bouton "partager"
+    btnPartage = document.querySelector(".zonePartage button")
+    let popupBackground = document.querySelector(".popupBackground")
+    btnPartage.addEventListener("click", () => {
+        // Quand on a cliqué sur le bouton partagé, on affiche la popup
+        afficherPopup()
+    })
+
+    // On écoute le click sur la div "popupBackground"
+    popupBackground.addEventListener("click", (event) => {
+        // Si on a cliqué précisément sur la popupBackground 
+        // (et pas un autre élément qui se trouve dedant)
+        if (event.target === popupBackground) {
+            // Alors on cache la popup
+            cacherPopup()
+        }
+    })
+}
